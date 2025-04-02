333af487875a464a597f587d8ac3eda4a4a9913a
Abdul Rahman <abdulnsamba@gmail.com>
2025-04-02 09:47:14 +0300

    added tailwind and react-router

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="bg-primary text-green-200 p-2">
+      <div>
+        <h1>MOFI</h1>
+      </div>
+      <div>
+        <ul>
+          <li>About</li>
+          <li>Compare</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
